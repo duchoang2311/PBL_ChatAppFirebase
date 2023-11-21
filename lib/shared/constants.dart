aa1db10d6e2035b41a30f5b20c732d599a8bfde6
duchoang2311 <nguyenduchoangct@gmail.com>
2023-11-21 15:14:24 +0700

    first

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:145851637851:web:590de7d637bc5a9df1df7e";
+  static String apiKey = "AIzaSyCdqRkuN7Hj4IjkFsv7p2uXqV6R3ubitjI";
+  static String messagingSenderId = "145851637851";
+  static String projectId = "chatappflutter-cbe44";
+}
